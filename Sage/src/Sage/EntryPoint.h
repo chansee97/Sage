7c1b795cf744d6c00872ac1a55717a775760ebc2
chansee97 <chen.dev@foxmail.com>
2025-06-06 23:40:20 +0800

    feat: add premake build system

--- Sage/src/Sage/EntryPoint.h
+++ Sage/src/Sage/EntryPoint.h
@@ -7,10 +7,8 @@ extern Sage::Application* Sage::CreateApplication();
 int main(int argc, char** argv)
 {	
 	Sage::Log::Init();
-	/*Sage::Log::GetCoreLogger()->warn("Initialized Log");
-	Sage::Log::GetClientLogger()->info("Hello");*/
 	SAGE_CORE_WARN("Initialized Log");
-	int a = 5;
+	int a = 10;
 	SAGE_INFO("Hello, Var = {0}", a);
 
 	auto app = Sage::CreateApplication();
---
+++ premake5.lua
@@ -0,0 +1,118 @@
+workspace "Sage"
+    architecture "x64"
+    configurations 
+    { 
+      "Debug", 
+      "Release",
+      "Dist"
+     }
+    platforms 
+    { 
+      "x64" 
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Sage"
+    location "Sage"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+    objdir ("bin-int/"..outputdir.. "/%{prj.name}")
+
+    files 
+    { 
+      "%{prj.name}/src/**.h", 
+      "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+      "%{prj.name}/vendor/spdlog/include",
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0.26100.0"
+        buildoptions 
+        { 
+          "/utf-8"
+        }
+
+        defines
+        {
+          "SAGE_PLATFORM_WINDOWS",
+          "SAGE_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+          ("{COPYDIR} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir ..  "/SandBox/\"" )
+        }
+    
+    filter "configurations:Debug"
+        defines "SAGE_DEBUG"
+        symbols "On"
+      
+    filter "configurations:Release"
+        defines "SAGE_RELEASE"
+        optimize "On"
+    
+    filter "configurations:Dist"
+        defines "SAGE_DIST"
+        optimize "On"
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+    objdir ("bin-int/"..outputdir.. "/%{prj.name}")
+
+    
+    files 
+    { 
+      "%{prj.name}/src/**.h", 
+      "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+      "Sage/vendor/spdlog/include",
+      "Sage/src",
+    }
+
+    links
+    {
+      "Sage"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0.26100.0"
+        buildoptions 
+        { 
+          "/utf-8"
+        }
+
+        defines
+        {
+          "SAGE_PLATFORM_WINDOWS",
+        }
+    
+    filter "configurations:Debug"
+        defines "SAGE_DEBUG"
+        symbols "On"
+      
+    filter "configurations:Release"
+        defines "SAGE_RELEASE"
+        optimize "On"
+    
+    filter "configurations:Dist"
+        defines "SAGE_DIST"
+        optimize "On"
